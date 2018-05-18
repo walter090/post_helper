a5946c7f4d8f882d6e8b263a90fe8970c9ae1173
walter090 <walter.wu090@gmail.com>
2018-05-18 04:41:51 -0400

    Change value of textarea on update instead of innerhtml

--- js/scripts.js
+++ js/scripts.js
@@ -16,47 +16,47 @@ function selectAndCopy(event) {
 function onUpdate(event) {
 	event.preventDefault();
 
-	let related = structure['related'] ? (`&lt;span class="dl"&gt;Related videos:&lt;/span&gt;
-&lt;ul class="dl-list"&gt;
- 	&lt;li&gt;&lt;a href="` + structure['related'] + `"&gt;` + structure['relatedname'] + `&lt;/a&gt;&lt;/li&gt;
-&lt;/ul&gt;`) : '';
+	let related = structure['related'] ? (`<span class="dl">Related videos:</span>
+<ul class="dl-list">
+ 	<li><a href="` + structure['related'] + `">` + structure['relatedname'] + `</a></li>
+</ul>`) : '';
 
-	let download = structure['download'] ? (`&lt;span class="dl"&gt;Download content here:&lt;/span&gt;
-&lt;ul class="dl-list"&gt;
- 	&lt;li&gt;&lt;a href="` + structure['download'] + `"&gt;` + structure['downloadname'] + `&lt;/a&gt;&lt;/li&gt;
-&lt;/ul&gt;`) : '';
+	let download = structure['download'] ? (`<span class="dl">Download content here:</span>
+<ul class="dl-list">
+ 	<li><a href="` + structure['download'] + `">` + structure['downloadname'] + `</a></li>
+</ul>`) : '';
 
-	let author = structure['authorlink'] ? (`&lt;li&gt;Author: &lt;a href="`+ structure['authorlink'] +`"&gt;` + structure['author'] + `&lt;/a&gt;&lt;/li&gt;`) : 
-	('&lt;li&gt;Author: &lt;span&gt;' + structure['author'] + '&lt;/span&gt;&lt;/li&gt;');
+	let author = structure['authorlink'] ? (`<li>Author: <a href="`+ structure['authorlink'] +`">` + structure['author'] + `</a></li>`) : 
+	('<li>Author: <span>' + structure['author'] + '</span></li>');
 
-	let photo = structure['photos'] ? (`&lt;li&gt;Photos: &lt;a href="` + structure['photos'] + `"&gt;` + structure['photos'] + `&lt;/a&gt;&lt;/li&gt;`) : '';
+	let photo = structure['photos'] ? (`<li>Photos: <a href="` + structure['photos'] + `">` + structure['photos'] + `</a></li>`) : '';
 
-	let sponsor = structure['sponsor'] ? (`&lt;li&gt;Sponsor: 
-&lt;a href="` + structure['sponsorsite'] + `"&gt;&lt;img src="` + structure['sponsor'] + `" alt="" width="300" height="164" class="alignnone size-medium wp-image-7105" /&gt;&lt;/a&gt;
-        &lt;/li&gt;`) : '';
+	let sponsor = structure['sponsor'] ? (`<li>Sponsor: 
+<a href="` + structure['sponsorsite'] + `"><img src="` + structure['sponsor'] + `" alt="" width="300" height="164" class="alignnone size-medium wp-image-7105" /></a>
+        </li>`) : '';
 
 	let embed = structure['embed'] ? (`[embedyt]` + structure['embed'] + `[/embedyt]`) : '';
 
-	let submit = `&lt;div class="parent"&gt;
-&lt;div class="left-side"&gt;
-&lt;div class="left-head"&gt;More by ` + structure['more'] + `&lt;/div&gt;
+	let submit = `<div class="parent">
+<div class="left-side">
+<div class="left-head">More by ` + structure['more'] + `</div>
 [display-posts author="` + structure['morecode'] + `" posts_per_page="5" category="Blog"]
 
-&lt;/div&gt;
-&lt;div class="middle"&gt;&lt;span class="contributors"&gt;Contributor: &lt;/span&gt;&lt;a href="/contributor/` + structure['contributorid'] + `/"&gt;`
-+ structure['contributor'] + `&lt;/a&gt;` + `&lt;br/&gt;` + `&lt;br/&gt;`
-+ structure['content'] + `&lt;br/&gt;` + embed + `&lt;br/&gt;` + related + download + `
-&lt;/div&gt;
-&lt;div class="right-side"&gt;
-&lt;ul&gt;
+</div>
+<div class="middle"><span class="contributors">Contributor: </span><a href="/contributor/` + structure['contributorid'] + `/">`
++ structure['contributor'] + `</a>` + `<br/>` + `<br/>`
++ structure['content'] + `<br/>` + embed + `<br/>` + related + download + `
+</div>
+<div class="right-side">
+<ul>
  	` + author + `
- 	&lt;li&gt;Editor: ` + structure['editor'] + `&lt;/li&gt;
- 	&lt;li&gt;Graphics Design: ` + structure['designer'] + `&lt;/li&gt;
+ 	<li>Editor: ` + structure['editor'] + `</li>
+ 	<li>Graphics Design: ` + structure['designer'] + `</li>
  	` + photo + sponsor + `
-&lt;/ul&gt;
-&lt;/div&gt;
-&lt;/div&gt;`;
+</ul>
+</div>
+</div>`;
 
-	document.getElementById('html-output').innerHTML = submit;
+	document.getElementById('html-output').value = submit;
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
