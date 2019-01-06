d3b936939352e31943cc713a16b8a6835843ffff
walterwu <walter.wu@zumepizza.com>
2019-01-06 13:36:19 -0800

    Update formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,60 @@
 let structure = {};
 
 function onChange(event) {
-	let id = event.id;
+    let id = event.id;
     structure[id] = event.value;
 }
 
 function onSwitch(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	let id = '#' + event.target.id;
+    let id = '#' + event.target.id;
 
-	if (!$(id).hasClass('active')) {
-		cleanSlate();
-		$(id).addClass('active');
-		$(id).siblings().removeClass('active');
+    if (!$(id).hasClass('active')) {
+        cleanSlate();
+        $(id).addClass('active');
+        $(id).siblings().removeClass('active');
 
-		let associated = '#' + $(id).attr('for');
-		$(associated).removeClass('inactive');
-		$(associated).siblings('.form-wrap').addClass('inactive');
-	}
+        let associated = '#' + $(id).attr('for');
+        $(associated).removeClass('inactive');
+        $(associated).siblings('.form-wrap').addClass('inactive');
+    }
 }
 
 function cleanSlate() {
-	let fields = document.getElementsByTagName('input');
-	let areas = document.getElementsByTagName('textarea');
+    let fields = document.getElementsByTagName('input');
+    let areas = document.getElementsByTagName('textarea');
 
-	fields = Array.from(fields);
-	areas = Array.from(areas);
+    fields = Array.from(fields);
+    areas = Array.from(areas);
 
-	fields.map(field => {
-		field.value = '';
-	});
-	areas.map(area => {
-		area.value = '';
-	});
+    fields.map(field => {
+        field.value = '';
+    });
+    areas.map(area => {
+        area.value = '';
+    });
 
-	structure = {};
+    structure = {};
 }
 
 function onClear(event) {
-	event.preventDefault();
-	cleanSlate();
+    event.preventDefault();
+    cleanSlate();
 }
 
 function selectAndCopy(event) {
-	// event.preventDefault();
+    // event.preventDefault();
 
-	let generated = document.getElementById('html-output');
-	generated.select();
-	document.execCommand('copy');
+    let generated = document.getElementById('html-output');
+    generated.select();
+    document.execCommand('copy');
 }
 
 function onUpdateContrib(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	let submit = `
+    let submit = `
 <div id="member">
 	<div id="intro">
 		` + structure['contrib-intro'] + `
@@ -67,47 +67,49 @@ function onUpdateContrib(event) {
 		</div>
 </div>
 	`;
-	document.getElementById('html-output').value = submit;
+    document.getElementById('html-output').value = submit;
 }
 
 function onUpdate(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	let related = structure['related'] ? (`<span class="dl">Related videos:</span>
+    let related = structure['related'] ? (`<span class="dl">Related videos:</span>
 <ul class="dl-list">
  	<li><a href="` + structure['related'] + `">` + structure['relatedname'] + `</a></li>
 </ul>`) : '';
 
-	let download = structure['download'] ? (`<span class="dl">Resources:</span>
+    let download = structure['download'] ? (`<span class="dl">Resources:</span>
 <ul class="dl-list">
  	<li><a href="` + structure['download'] + `">` + structure['downloadname'] + `</a></li>
 </ul>`) : '';
 
-	let author = structure['authorlink'] ? (`<li>Author: <a href="`+ structure['authorlink'] +`">` + structure['author'] + `</a></li>`) : 
-	('<li>Author: <span>' + structure['author'] + '</span></li>');
+    let author = structure['authorlink'] ? (`<li>Author: <a href="` + structure['authorlink'] + `">` + structure['author'] + `</a></li>`) :
+        ('<li>Author: <span>' + structure['author'] + '</span></li>');
 
-	let editor = structure['editorlink'] ? (`<li>Editor: <a href="`+ structure['editorlink'] +`">` + structure['editor'] + `</a></li>`) : 
-	('<li>Editor: <span>' + structure['editor'] + '</span></li>');
+    let editor = structure['editorlink'] ? (`<li>Editor: <a href="` + structure['editorlink'] + `">` + structure['editor'] + `</a></li>`) :
+        ('<li>Editor: <span>' + structure['editor'] + '</span></li>');
 
-	let photo = structure['photos'] ? (`<li>Photos: <a href="` + structure['photoslink'] + `">` + structure['photos'] + `</a></li>`) : '';
+    let photo = structure['photos'] ? (`<li>Photos: <a href="` + structure['photoslink'] + `">` + structure['photos'] + `</a></li>`) : '';
 
-	let sponsor = structure['sponsor'] ? (`<li>Sponsor: 
+    let sponsor = structure['sponsor'] ? (`<li>Sponsor: 
 <a href="` + structure['sponsorsite'] + `"><img src="` + structure['sponsor'] + `" alt="" width="300" height="164" class="alignnone size-medium wp-image-7105" /></a>
         </li>`) : '';
 
-	let embed = structure['embed'] ? (`[embedyt]` + structure['embed'] + `[/embedyt]`) : '';
+    let embed = structure['embed'] ? (`[embedyt]` + structure['embed'] + `[/embedyt]`) : '';
 
-	let designer = structure['designer'] ? (`<li>Graphics Design: ` + structure['designer'] + `</li>`) : ''
+    let designer = structure['designer'] ? (`<li>Graphics Design: ` + structure['designer'] + `</li>`) : ''
 
-	let submit = `<div class="parent">
+    let submit = `<div class="parent">
 <div class="left-side">
 <div class="left-head">More by ` + structure['more'] + `</div>
 [display-posts tag="` + structure['morecode'] + `" posts_per_page="5"]
 
 </div>
-<div class="middle"><span class="contributors">Contributor: </span><a href="/contributor/` + structure['contributorid'] + `/">`
-+ structure['contributor'] + `</a>` + `<br/>` + `<br/>`
-+ structure['content'] + `<br/>` + embed + `<br/>` + related + download + `
+<div class="middle">
+<span class="contributors">Contributor: </span>
+<a href="/contributor/` + structure['contributorid'] + `/">`
+        + structure['contributor'] + `</a>` + `<br/>` + `<br/>`
+        + structure['content'] + `<br/>` + embed + `<br/>` + related + download + `
 </div>
 <div class="right-side">
 <ul>
@@ -120,6 +122,6 @@ function onUpdate(event) {
 </div>
 </div>`;
 
-	document.getElementById('html-output').value = submit;
-	document.body.scrollTop = document.documentElement.scrollTop = 0;
+    document.getElementById('html-output').value = submit;
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
